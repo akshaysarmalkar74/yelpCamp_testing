a7ec7aebc35b4799007a5e0665d9a167de9b9a43
01Ani <95162490+01Ani@users.noreply.github.com>
2024-11-05 00:02:49 +0530

    Add new campgrounds-this is after finding each campground by id

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const mongoose = require("mongoose");
 const path = require("path");
 const app = express();
 const Campground = require("./models/campground");
+const campground = require("./models/campground");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({ extended: true }));
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/yelp-camp")
@@ -32,6 +35,16 @@ app.get("/", (req, res) => {
 app.get("/campgrounds", async (req, res) => {
   const campgrounds = await Campground.find({});
   res.render("campgrounds/index", { campgrounds });
+});
+
+app.post("/campgrounds", async (req, res) => {
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+
+app.get("/campgrounds/new", (req, res) => {
+  res.render("campgrounds/new");
 });
 
 app.get("/campgrounds/:id", async (req, res) => {
