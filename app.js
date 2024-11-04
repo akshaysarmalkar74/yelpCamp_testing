8b136f8faaada53d7c6c9d9e306d6c7e498b83ed
01Ani <95162490+01Ani@users.noreply.github.com>
2024-11-04 21:53:14 +0530

    added campground in views directory and made an li of titles

--- app.js
+++ app.js
@@ -16,17 +16,22 @@ mongoose
     console.log("THERE WAS AN ERROR ON MONGO", err);
   });
 
-app.get("/makecampground", async (req, res) => {
-  const camp = new Campground({
-    title: "My Backyard",
-    description: "cheap camping",
-  });
-  await camp.save();
-  res.send(camp);
-});
+// app.get("/makecampground", async (req, res) => {
+//   const camp = new Campground({
+//     title: "My Backyard",
+//     description: "cheap camping",
+//   });
+//   await camp.save();
+//   res.send(camp);
+// });
 
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.get("/campgrounds", async (req, res) => {
+  const campgrounds = await Campground.find({});
+  res.render("campgrounds/index", { campgrounds });
 });
 
 app.listen(3000, () => {
