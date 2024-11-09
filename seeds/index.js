f4b94a1cbb19a9a04be9384be9495ce09dea2c24
01Ani <95162490+01Ani@users.noreply.github.com>
2024-11-09 13:20:13 +0530

    added images to seed campground and installed axios to help seed image

--- seeds/index.js
+++ seeds/index.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const axios = require("axios");
 const Campground = require("../models/campground");
 const cities = require("./cities");
 const { places, descriptors } = require("./seedHelpers");
@@ -18,13 +19,33 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: await seedImg(),
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem totam, hic, repellat ipsum tempore consectetur eos officiis necessitatibus impedit error dicta sapiente quod nesciunt, vitae odit quas. Aliquid, illo autem?",
+      price, //short hand instead of price:price
     });
     await camp.save();
   }
 };
+
+// call unsplash and return small image
+async function seedImg() {
+  try {
+    const resp = await axios.get("https://api.unsplash.com/photos/random", {
+      params: {
+        client_id: "tuFbT24bB7aSenE0KaNpgioEJKFNTffbUjeLaZY4_Pk",
+        collections: 1114848,
+      },
+    });
+    return resp.data.urls.small;
+  } catch (err) {
+    console.error(err);
+  }
+}
 
 seedDB().then(() => {
   mongoose.connection.close();
