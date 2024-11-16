b645b82c23d28e177841a29619269650a704f88c
01Ani <95162490+01Ani@users.noreply.github.com>
2024-11-16 20:27:44 +0530

    defining expressError.js and catchAsync.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const path = require("path");
 const app = express();
 const ejsMate = require("ejs-mate");
+const catchAsync = require("./utils/catchAsync");
 const methodOverride = require("method-override");
 const Campground = require("./models/campground");
 
@@ -34,55 +35,73 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.get("/campgrounds", async (req, res) => {
-  const campgrounds = await Campground.find({});
-  res.render("campgrounds/index", { campgrounds });
-});
+app.get(
+  "/campgrounds",
+  catchAsync(async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render("campgrounds/index", { campgrounds });
+  })
+);
 
 //for getting the id of the particular li and redirecting to that page
 //for creating new campground
-app.post("/campgrounds", async (req, res, next) => {
-  try {
-    const campground = new Campground(req.body.campground);
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
-  } catch (e) {
-    next(e);
-  }
-});
+app.post(
+  "/campgrounds",
+  catchAsync(async (req, res, next) => {
+    try {
+      const campground = new Campground(req.body.campground);
+      await campground.save();
+      res.redirect(`/campgrounds/${campground._id}`);
+    } catch (e) {
+      next(e);
+    }
+  })
+);
 
 //remember that we cannot put /new below /:id because if done so, /new will be considered as if given to /:id
 app.get("/campgrounds/new", (req, res) => {
   res.render("campgrounds/new");
 });
 
-app.get("/campgrounds/:id", async (req, res) => {
-  const campground = await Campground.findById(req.params.id);
-  res.render("campgrounds/show", { campground });
-});
+app.get(
+  "/campgrounds/:id",
+  catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render("campgrounds/show", { campground });
+  })
+);
 
-app.get("/campgrounds/:id/edit", async (req, res) => {
-  const campground = await Campground.findById(req.params.id);
-  res.render("campgrounds/edit", { campground });
-});
+app.get(
+  "/campgrounds/:id/edit",
+  catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render("campgrounds/edit", { campground });
+  })
+);
 
-app.put("/campgrounds/:id", async (req, res) => {
-  const { id } = req.params;
-  const campground = await Campground.findByIdAndUpdate(id, {
-    ...req.body.campground,
-  }); //spread operator instead of keeping it in one line it spreads into different ones like -
-  //{
-  //title:
-  //location:
-  //}
-  res.redirect(`/campgrounds/${campground._id}`);
-});
+app.put(
+  "/campgrounds/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, {
+      ...req.body.campground,
+    }); //spread operator instead of keeping it in one line it spreads into different ones like -
+    //{
+    //title:
+    //location:
+    //}
+    res.redirect(`/campgrounds/${campground._id}`);
+  })
+);
 
-app.delete(`/campgrounds/:id`, async (req, res) => {
-  const { id } = req.params;
-  await Campground.findByIdAndDelete(id);
-  res.redirect("/campgrounds");
-});
+app.delete(
+  `/campgrounds/:id`,
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect("/campgrounds");
+  })
+);
 
 app.use((err, req, res, next) => {
   res.send("SOMETHING WENT WRONG!");
---
+++ utils/ExpressError.js
@@ -0,0 +1,7 @@
+class ExpressError extends Error {
+  constructor(message, statusCode) {
+    this.message = message;
+    this.statusCode = statusCode;
+  }
+}
+module.exports = ExpressError;
