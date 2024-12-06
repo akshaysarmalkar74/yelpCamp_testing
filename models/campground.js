1a25cdff0c5b997f1c7200433ef7ac1264e45107
01Ani <95162490+01Ani@users.noreply.github.com>
2024-12-06 11:54:07 +0530

    Made review.js model and added a new schemaType to Campground.js with ref of 'Review'

--- models/campground.js
+++ models/campground.js
@@ -7,6 +7,13 @@ const CampgroundSchema = new mongoose.Schema({
   price: Number,
   description: String,
   location: String,
+  // below is added later after creating review.js model, one campoground has many reviews
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
 });
 
 module.exports = mongoose.model("Campground", CampgroundSchema);
