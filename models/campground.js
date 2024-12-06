7180fe7e0fab524208e569feb0fdce55e4d84b01
01Ani <95162490+01Ani@users.noreply.github.com>
2024-12-06 21:13:37 +0530

    adding mongoose middleware in campground.js to basically delete all the reviews after deleting the campground itself

--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const { campgroundSchema } = require("../schemas");
+const Review = require("./review");
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new mongoose.Schema({
@@ -14,6 +16,20 @@ const CampgroundSchema = new mongoose.Schema({
       ref: "Review",
     },
   ],
+});
+
+//for deleting reviews as well when we delete campground...we use the mongoose middleware as follows, use findOneAndDelete.
+
+CampgroundSchema.post("findOneAndDelete", async function (doc) {
+  if (doc) {
+    //this document has reviews, we will delete all reviews where their id field is in our document(doc) that was just deleted
+    await Review.deleteMany({
+      //id for each review is somewhere in document.reviews
+      _id: {
+        $in: doc.reviews,
+      },
+    });
+  }
 });
 
 module.exports = mongoose.model("Campground", CampgroundSchema);
