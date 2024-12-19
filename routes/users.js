293485aa7279c8c135bcd35e431403600d464dab
01Ani <95162490+01Ani@users.noreply.github.com>
2024-12-19 18:08:16 +0530

    added login routes and view page form, used post.authenticate() for the first time

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 const catchAsync = require("../utils/catchAsync");
+const passport = require("passport");
 
 router.get("/register", (req, res) => {
   res.render("users/register");
@@ -13,8 +14,8 @@ router.post(
     try {
       const { email, username, password } = req.body;
       const user = new User({ email, username });
-      const registeredUser = await User.register(user, password); //this is from passport, hashes and salts it.
-      console.log(registeredUser);
+      const registeredUser = await User.register(user, password); //this is from passport, hashes and salts it. and also User.save() is done automatically through it, no need to specify
+      //   console.log(registeredUser);
       req.flash("success", "Welcome to yelpCamp");
       res.redirect("/campgrounds");
     } catch (e) {
@@ -22,6 +23,23 @@ router.post(
       res.redirect("/register");
     }
   })
+);
+
+router.get("/login", (req, res) => {
+  res.render("users/login");
+});
+
+//we use passport to easily do this
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureFlash: true,
+    failureRedirect: "/login",
+  }),
+  (req, res) => {
+    req.flash("success", "Welcome Back!");
+    res.redirect("/campgrounds");
+  }
 );
 
 module.exports = router;
