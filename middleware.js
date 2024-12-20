bd13bb850dcb600e2d88929cad2163a7904153ba
01Ani <95162490+01Ani@users.noreply.github.com>
2024-12-20 15:04:23 +0530

    We add another middleware to middleware.js so that we can return to the particular page we were on before we logged in

--- middleware.js
+++ middleware.js
@@ -1,11 +1,20 @@
 module.exports.isLoggedIn = (req, res, next) => {
-  console.log("REQ.USER", req.user); //req.user is coming from the session, which is deserialised to give back who the user is that is stored
+  //console.log("REQ.USER", req.user); //req.user is coming from the session, which is deserialised to give back who the user is that is stored
   //above gives back id, username and email
   if (!req.isAuthenticated()) {
     //basically we do the following if we still aren't authenticated but try to hit this route still
     //isAuthenticated() is coming from passport
+    //console.log(req.path, req.originalUrl); //shows the url that you are visiting before logging in
+    req.session.returnTo = req.originalUrl; //for returning to the originalUrl that we were at before logging in
     req.flash("error", "You must be signed in");
     return res.redirect("/login");
+  }
+  next();
+};
+
+module.exports.storeReturnTo = (req, res, next) => {
+  if (req.session.returnTo) {
+    res.locals.returnTo = req.session.returnTo;
   }
   next();
 };
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require("../models/user");
 const catchAsync = require("../utils/catchAsync");
 const passport = require("passport");
+const { storeReturnTo } = require("../middleware"); //from middleware.js
 
 router.get("/register", (req, res) => {
   res.render("users/register");
@@ -36,15 +37,25 @@ router.get("/login", (req, res) => {
 //we use passport to easily do this
 router.post(
   "/login",
+  // use the storeReturnTo middleware to save the returnTo value from session to res.locals
+  storeReturnTo,
+  // passport.authenticate logs the user in and clears req.session
   passport.authenticate("local", {
     failureFlash: true,
     failureRedirect: "/login",
   }),
+  // Now we can use res.locals.returnTo to redirect the user after login
   (req, res) => {
-    req.flash("success", "Welcome Back!");
-    res.redirect("/campgrounds");
+    req.flash("success", "Welcome back!");
+    const redirectUrl = res.locals.returnTo || "/campgrounds"; // update this line to use res.locals.returnTo now
+    delete req.session.returnTo; //we dont want the returnTo variable just to sit in the session after you go to that page, so we delete it after you enter that page
+    res.redirect(redirectUrl);
   }
 );
+/*NOTE FOR ABOVE:
+By using the storeReturnTo middleware function, we can save the returnTo value to res.locals before passport.authenticate() clears the 
+session and deletes req.session.returnTo. This enables us to access and use the returnTo value (via res.locals.returnTo) 
+later in the middleware chain so that we can redirect users to the appropriate page after they have logged in. */
 
 //through the help of passport
 router.get("/logout", (req, res, next) => {
