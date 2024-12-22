5f9f3cd212197e5985e97bec963bc5ab1fecefdf
01Ani <95162490+01Ani@users.noreply.github.com>
2024-12-22 18:49:45 +0530

    adding campground permissions to not be able to access particular unauthorized routes on the URL if you are not the creator, later moved to middleware.

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -93,10 +93,16 @@ router.get(
   "/:id/edit",
   isLoggedIn,
   catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
     if (!campground) {
       req.flash("error", "Campground does not exist, cannot edit!");
       res.redirect("/campgrounds");
+    }
+
+    if (!campground.author.equals(req.user._id)) {
+      req.flash("error", "You do not have permission to do that");
+      return res.redirect(`/campgrounds/${id}`);
     }
     res.render("campgrounds/edit", { campground });
   })
@@ -108,7 +114,12 @@ router.put(
   validateCampground,
   catchAsync(async (req, res) => {
     const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, {
+    const campground = await Campground.findById(id);
+    if (!campground.author.equals(req.user._id)) {
+      req.flash("error", "You do not have permission to do that");
+      return res.redirect(`/campgrounds/${id}`);
+    }
+    const camp = await Campground.findByIdAndUpdate(id, {
       ...req.body.campground,
     }); //spread operator instead of keeping it in one line it spreads into different ones like -
     //{
