b96386e45c52fb27eb476b1afc12b58d69d6bbad
01Ani <95162490+01Ani@users.noreply.github.com>
2025-01-05 23:16:06 +0530

    Improved map feature to view actual location
    The campground can now be viewed for the actual location
    set when the campground is created.

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,8 +1,18 @@
 maptilersdk.config.apiKey = maptilerApiKey;
 
 const map = new maptilersdk.Map({
-  container: "map",
+  container: "map", //is referring to the div we set in show page with id="map"
   style: maptilersdk.MapStyle.BRIGHT,
-  center: [-74.5, 40], // starting position [lng, lat]
+  center: campground.geometry.coordinates, // starting position [lng, lat]
   zoom: 10, // starting zoom
 });
+
+//below is used to give a pointer(marker) to the exact coordinates
+new maptilersdk.Marker()
+  .setLngLat(campground.geometry.coordinates)
+  .setPopup(
+    new maptilersdk.Popup({ offset: 25 }).setHTML(
+      `<h3>${campground.title}</h3><p>${campground.location}</p>`
+    )
+  )
+  .addTo(map);
